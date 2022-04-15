321193457ba783906612a49dc9499152c45fa1b3
Souhail Abdennebi <100965022+souhail210301@users.noreply.github.com>
2022-04-15 01:16:06 +0100

    Add files via upload

---
+++ TP4_produits/TP4/src/main/java/com/example/produits/ProduitApplication.java
@@ -0,0 +1,26 @@
+package com.example.produits;
+
+import java.util.Date;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.produits.entities.Produit;
+import com.example.produits.service.ProduitService;
+
+@SpringBootApplication
+public class ProduitApplication implements CommandLineRunner {
+@Autowired
+ProduitService produitService;
+public static void main(String[] args) {
+SpringApplication.run(ProduitApplication.class, args);
+}
+@Override
+public void run(String... args) throws Exception {
+produitService.saveProduit(new Produit("PC Dell", 2600.0, new Date()));
+produitService.saveProduit(new Produit("PC Asus", 2800.0, new Date()));
+produitService.saveProduit(new Produit("Imprimante Epson", 900.0, new Date()));
+}}
